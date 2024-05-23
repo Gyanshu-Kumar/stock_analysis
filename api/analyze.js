cc040fb9fd158ae81a1159968ff5a3ea51949a76
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-05-23 18:08:38 +0530

    commit

--- api/analyze.js
+++ api/analyze.js
@@ -4,23 +4,15 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
-
-// Use the full path to the Python executable, wrapped in quotes
 const pythonPath = '"C:\\Users\\GYANSHU KUMAR\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.post('/analyze', (req, res) => {
+app.post('/api/analyze', (req, res) => {
     const { year } = req.body;
 
-    const command = `${pythonPath} python/analyze_stocks.py ${year}`;
+    const command = `${pythonPath} ../python/analyze_stocks.py ${year}`;
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
@@ -35,6 +27,4 @@ app.post('/analyze', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
