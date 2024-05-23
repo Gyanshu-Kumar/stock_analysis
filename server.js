c12553b96353b44ed7fb8d04cbe2c9cc23b47caf
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-05-23 18:02:19 +0530

    commit

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-
+// Use the full path to the Python executable, wrapped in quotes
 const pythonPath = '"C:\\Users\\GYANSHU KUMAR\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
